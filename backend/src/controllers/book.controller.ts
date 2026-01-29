0d66dc5d79952be8cd724203596c65a65024b321
Gian Neves <gianneves@hotmail.com>
2026-01-29 18:16:17 -0300

    add: book controller

--- backend/src/controllers/book.controller.ts
+++ backend/src/controllers/book.controller.ts
@@ -17,13 +17,29 @@ export const bookController = {
         }
     },
 
+    getAllCategoriers: async (req: Request, res: Response) => {
+        try {
+            const category = await prisma.book.findMany({
+                select: {
+                    category: true
+                }
+            })
+
+            if (!category) return res.status(404).json({ message: "Nenhum livro encontrado" })
+
+            return res.status(200).json({ success: true, category })
+        } catch(e) {
+
+        }
+    },
+
     getBooksByCategory: async (req: Request, res: Response) => {
         try {
             const { category } = req.params
             if (!category) return res.status(404).json({ message: "Categoria não encontrada" })
 
             const books = await prisma.book.findMany({
-                select: { 
+                select: {
                     id: true,
                     title: true,
                     description: true
@@ -35,6 +51,23 @@ export const bookController = {
 
             res.status(200).json({ books })
 
+        } catch (e) {
+            console.error("Erro interno")
+            res.status(500).json({ message: "Erro interno!" })
+        }
+    },
+
+    getAllPrices: async (req: Request, res: Response) => {
+        try {
+            const prices = await prisma.book.findMany({
+                select: {
+                    price: true
+                }
+            })
+
+            if (!prices) return res.status(404).json({ message: "Preços não encontrados" })
+
+            return res.status(200).json({ success: true, prices })
         } catch (e) {
             console.error("Erro interno")
             res.status(500).json({ message: "Erro interno!" })
