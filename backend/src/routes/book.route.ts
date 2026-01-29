dcb5bbe29ad02fe95830fb5400f55fdfee761d31
Gian Neves <gianneves@hotmail.com>
2026-01-29 18:14:09 -0300

    add: routes

--- backend/src/routes/book.route.ts
+++ backend/src/routes/book.route.ts
@@ -4,4 +4,6 @@ import { bookController } from "../controllers/book.controller.ts";
 export const bookRoutes = Router()
 
 bookRoutes.get('/', bookController.getBooks)
+bookRoutes.get('/prices', bookController.getAllPrices)
 bookRoutes.get('/categories/:category', bookController.getBooksByCategory)
+bookRoutes.get('/categories', bookController.getAllCategoriers)
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { userController } from "../controllers/user.controller.ts";
+import { authMiddleware } from "../middleware/auth.ts";
 
 export const userRoutes = Router()
 
+userRoutes.get('/user', authMiddleware,userController.getOneUser)
 userRoutes.post('/', userController.createUser)
 userRoutes.get('/', userController.getUsers)
